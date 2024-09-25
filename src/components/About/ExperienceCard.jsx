93ec1b3aa9f73c4ccc36cef1d4f6d397938de661
Andrew Cheung <andrewcheung360@gmail.com>
2024-09-25 01:01:00 -0400

    Added appdev experience

--- src/components/About/ExperienceCard.jsx
+++ src/components/About/ExperienceCard.jsx
@@ -6,14 +6,31 @@ export default function ExperienceCard({isMobile}) {
     return(
         <>
         {isMobile ?
-        <div className = "w-full h-1/5 rounded-3xl bg-[#101010] flex flex-col items-center py-6 gap-y-8 ring-1 ring-[#fffcf0]">
+        <div className = "w-full h-1/5 rounded-3xl bg-[#101010] flex flex-col items-center py-6 gap-y-2 ring-1 ring-[#fffcf0]">
             <span className = "text-[#FFFCF0] text-[11vw] font-semibold">
                     EXPERIENCES
             </span>
             <div className = "w-full flex flex-col items-center gap-y-6">
                 <div className = "w-[90%] flex flex-row justify-between items-center text-[#fffcf0] ">
                     <div className = "flex flex-col">
-                        <span className = "text-[5vw] font-semibold">
+                        <span className = "text-[4vw] font-semibold">
+                            Cornell AppDev
+                        </span>
+                        <span className = "text-[3vw]">
+                            Android Developer
+                        </span>
+                    </div>
+                    <div>
+                        <span className = " text-[3vw] font-semibold">
+                            2024 - Now
+                        </span>
+                    </div>
+                </div>
+
+                <div className = "w-[90%] bg-[#fffcf0] h-[1px]"/>
+                <div className = "w-[90%] flex flex-row justify-between items-center text-[#fffcf0] ">
+                    <div className = "flex flex-col">
+                        <span className = "text-[4vw] font-semibold">
                             Zebra Technologies
                         </span>
                         <span className = "text-[3vw]">
@@ -31,7 +48,7 @@ export default function ExperienceCard({isMobile}) {
 
                 <div className = "w-[90%] flex flex-row justify-between items-center text-[#fffcf0] ">
                     <div className = "flex flex-col">
-                        <span className = "text-[5vw] font-semibold">
+                        <span className = "text-[4vw] font-semibold">
                             Cornell University
                         </span>
                         <span className = "text-[3vw]">
@@ -44,8 +61,6 @@ export default function ExperienceCard({isMobile}) {
                         </span>
                     </div>
                 </div>
-
-                <div className = "w-[90%] bg-[#fffcf0] h-[1px]"/>
 
 
             </div>
@@ -60,6 +75,22 @@ export default function ExperienceCard({isMobile}) {
         </div>
         
         <div className = "flex flex-col items-center h-full gap-y-10 xl:medium:gap-y-12 2xl:tall:gap-y-20 3xl:gap-y-16">
+            <div className = "w-full flex flex-row justify-between items-center text-[#fffcf0] ">
+                <div className = "flex flex-col">
+                    <span className = "text-[2vw] font-semibold">
+                        Cornell AppDev
+                    </span>
+                    <span className = "text-[1.25vw]">
+                        Android Developer
+                    </span>
+                </div>
+                <div>
+                    <span className = " text-[1.5vw] font-semibold">
+                        2024 - Now
+                    </span>
+                </div>
+            </div>
+            <div className = "w-full bg-[#fffcf0] h-[1px]"/>
             <div className = "w-full flex flex-row justify-between items-center text-[#fffcf0] ">
                 <div className = "flex flex-col">
                     <span className = "text-[2vw] font-semibold">
@@ -94,16 +125,14 @@ export default function ExperienceCard({isMobile}) {
                 </div>
             </div>
 
-            <div className = "w-full bg-[#fffcf0] h-[1px]"/>
+            {/* <div className = "w-full bg-[#fffcf0] h-[1px]"/> */}
 
-            <div className = "w-full flex flex-row justify-evenly gap-x-4 xl:gap-x-12 2xl:gap-x-6 items-center text-[#fffcf0]">
-                {/* <span className = "text-[1.5vw] font-medium w-1/2">
-                    For more information, check out my resume!
-                </span> */}
+            {/* <div className = "w-full flex flex-row justify-evenly gap-x-4 xl:gap-x-12 2xl:gap-x-6 items-center text-[#fffcf0]">
+
                 <LinkButton text = "RESUME" href = "https://drive.google.com/file/d/1i4_e0rPOXiEjo1hd_iooSgE_2jK3GmvY/view?usp=sharing"/>
                 <LinkButton text = "LINKEDIN" href = "https://www.linkedin.com/in/andrewcheung360/"/>
                 <LinkButton text = "GITHUB" href = "https://github.com/AndrewCheung360"/>
-            </div>
+            </div> */}
 
             </div>
         </div>
